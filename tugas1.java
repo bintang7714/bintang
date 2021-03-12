c7f032028b69c10b5a6a8d63caaea891fc52242d
bintang7714 <80254774+bintang7714@users.noreply.github.com>
2021-03-13 05:56:26 +0700

    Add files via upload

---
+++ tugas1.java
@@ -0,0 +1,19 @@
+//Bintang Kurniawan
+//A11.2019.12290
+package tugas1.java;
+
+public class tugas1 {
+
+	public static void main(String[] args) {
+	int a = 5, b = 8, c = 6, d = 9, e = 55, f = 20, g = 15, h = 3, i = 2 ;
+	int No1 = -a +b*c;
+	int No2 = (e+d)%d;
+	int No3 = f+(-h*a)/b;
+	int No4 = a+g/h*i-b%h;
+	System.out.println("A. " +No1);
+	System.out.println("B. " +No2);
+	System.out.println("C. " +No3);
+	System.out.println("D. " +No4);
+	}
+
+}
