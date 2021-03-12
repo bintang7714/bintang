70c680a791e62112a8939011bec7255242a1917c
bintang7714 <80254774+bintang7714@users.noreply.github.com>
2021-03-12 20:03:07 +0700

    Create TUGAS1.java

---
+++ TUGAS1.java
@@ -0,0 +1,18 @@
+// Bintang Kurniawan
+// A11.2019.12290
+
+public class TUGAS1{
+    public static void main (String[] args) {
+    int a = 5, b = 8, c = 6, d = 9, e = 55, f = 20, g = 15, h = 3, i = 2 ;
+    
+        int 1 = -a +b*c;
+        int 2 = (e+d)%d;
+        int 3 = f+(-h*a)/b;
+        int 4 = a+g/h*i-b%h;
+        
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+      }
+}
